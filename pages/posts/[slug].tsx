37e15af3cc7e08e63d43644610d651e959b8f893
yuuki330 <katsuyu107@gmail.com>
2023-10-22 19:33:08 +0900

    背景色の削除と不要な要素の削除

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -42,7 +42,7 @@ export default function Post({ post, morePosts, preview }: Props) {
           <PostTitle>Loading…</PostTitle>
         ) : (
           <>
-            <article className="mb-32">
+            <article className="pb-32">
               <Head>
                 <title>{title}</title>
                 <meta property="og:image" content={post.ogImage.url} />
